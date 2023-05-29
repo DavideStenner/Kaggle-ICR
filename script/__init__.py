d981d06f2b154e1ff2aef8003cd14642deb0ce5f
Davide Stenner <davide.stenner@cefriel.com>
2023-05-29 12:35:54 +0200

    import change

--- script/__init__.py
+++ script/__init__.py
@@ -1,15 +1,15 @@
 from script.preprocess import preprocess_data
 from script.utils import set_seed_globally
 
-from script.contrastive.lgb_model import run_lgb_experiment
-from script.contrastive.lgb_model import evaluate_lgb_score
+from script.contrastive.lgb_model import run_lgb_contrastive_experiment
+from script.contrastive.lgb_model import evaluate_contrastive_lgb_score
 
-from script.contrastive.xgb_model import run_xgb_experiment
-from script.contrastive.xgb_model import evaluate_xgb_score
+from script.contrastive.xgb_model import run_contrastive_xgb_experiment
+from script.contrastive.xgb_model import evaluate_contrastive_xgb_score
 
 __all__ = [
     "preprocess_data",
     "set_seed_globally", 
-    "run_lgb_experiment", "run_xgb_experiment",
-    "evaluate_lgb_score", "evaluate_xgb_score"
+    "run_lgb_contrastive_experiment", "run_contrastive_xgb_experiment",
+    "evaluate_contrastive_lgb_score", "evaluate_contrastive_xgb_score"
 ]
