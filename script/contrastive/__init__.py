e01788955092f7ad3d91d48bb428c9ae0abfb40e
Davide Stenner <davide.stenner@cefriel.com>
2023-05-29 12:37:38 +0200

    import change

--- script/contrastive/__init__.py
+++ script/contrastive/__init__.py
@@ -1,15 +1,15 @@
 from script.contrastive.augment import contrastive_pipeline, fe_pipeline, fe_new_col_name
 
-from script.contrastive.lgb_model import run_lgb_experiment
-from script.contrastive.lgb_model import evaluate_lgb_score
+from script.contrastive.lgb_model import run_lgb_contrastive_experiment
+from script.contrastive.lgb_model import evaluate_contrastive_lgb_score
 
-from script.contrastive.xgb_model import run_xgb_experiment
-from script.contrastive.xgb_model import evaluate_xgb_score
+from script.contrastive.xgb_model import run_contrastive_xgb_experiment
+from script.contrastive.xgb_model import evaluate_contrastive_xgb_score
 
 __all__=[
     "fe_new_col_name",
     "fe_pipeline",
     'contrastive_pipeline',
-    'run_lgb_experiment', 'evaluate_lgb_score',
-    'run_xgb_experiment', 'evaluate_xgb_score'
+    'run_lgb_contrastive_experiment', 'evaluate_contrastive_lgb_score',
+    'run_contrastive_xgb_experiment', 'evaluate_contrastive_xgb_score'
 ]
